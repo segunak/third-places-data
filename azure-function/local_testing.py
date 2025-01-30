46e97b531acfc10cc3df0ebbf5336773009a522a
Segun Akinyemi <akintundejr4@gmail.com>
2025-01-30 12:49:09 -0500

    adding azure function files

---
+++ azure-function/local_testing.py
@@ -0,0 +1,6 @@
+from airtable_client import AirtableClient
+
+# A file for adhoc testing of Azure Function related code.
+# This file is not part of the main codebase and is not used in production.
+
+airtable = AirtableClient()
