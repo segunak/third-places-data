a45a8aea496aac9fed9a3e5c4b1872d884fbaa01
Segun Akinyemi <akintundejr4@gmail.com>
2025-03-01 15:42:50 -0500

    Out with the old, in with the new.

--- azure-function-legacy/local_testing.py
+++
@@ -1,6 +0,0 @@
-from airtable_client import AirtableClient
-
-# A file for adhoc testing of Azure Function related code.
-# This file is not part of the main codebase and is not used in production.
-
-airtable = AirtableClient()
